7e82c43576c6a6db5ad201bf4a1d490485903f2d
const2 <magic.const@gmail.com>
2021-03-12 20:41:08 +0900

    템플릿을 이용하도록 홈페이지 뷰를 리팩터링

--- tdd/lists/tests.py
+++ tdd/lists/tests.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
@@ -14,10 +15,5 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         # home_page 뷰에 전달하여 응답을 취득한다.
         response = home_page(request)
-
-        # html로 시작하는지 확인
-        self.assertTrue(response.content.startswith(b'<html>'))
-        # 제목 일치하는지 확인
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
-        # html로 끝나는지 확인
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('lists/home.html')
+        self.assertEqual(response.content.decode(), expected_html)
