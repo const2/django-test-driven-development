8dbbb3aa6798c033905c2c08398735a6630a5050
const2 <magic.const@gmail.com>
2021-03-17 20:33:53 +0900

    POST 데이터 생성및 리디렉션 테스트

--- tdd/lists/tests.py
+++ tdd/lists/tests.py
@@ -16,7 +16,7 @@ class ItemModelTest(TestCase):
         second_item.text = "두 번째 아이템"
         second_item.save()
 
-        saved_items = Items.objects.all()
+        saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         first_saved_item = saved_items[0]
@@ -26,17 +26,23 @@ class ItemModelTest(TestCase):
 
 
 class HomePageTest(TestCase):
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_root_url_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_return_correct_html(self):
-        # 브라우저에게 보낼 요청 객체를 생성한다.
-        request = HttpRequest()
-        # home_page 뷰에 전달하여 응답을 취득한다.
-        response = home_page(request)
-        expected_html = render_to_string('lists/home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+    # csrf_token 다름
+    # def test_home_page_return_correct_html(self):
+    # 브라우저에게 보낼 요청 객체를 생성한다.
+    # request = HttpRequest()
+    # home_page 뷰에 전달하여 응답을 취득한다.
+    # response = home_page(request)
+    # expected_html = render_to_string('lists/home.html')
+    # self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -44,12 +50,17 @@ class HomePageTest(TestCase):
         request.POST['item_text'] = '신규 작업 아이템'
 
         response = home_page(request)
-        response_content = response.content.decode()
-        self.assertIn('신규 작업 아이템', response_content)
-        expected_html = render_to_string(
-            'lists/home.html',
-            request=request
-        )
-        print(expected_html)
-        # csrf token으로 인한 불일치..
-        # self.assertEqual(response_content.tr, expected_html)
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
--- tdd/lists/views.py
+++ tdd/lists/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Item
+
+
 def home_page(request):
-    return render(request, 'lists/home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == "POST":
+        Item.objects.create(text=request.POST.get("item_text"))
+        return redirect('/')
+
+    return render(request, 'lists/home.html')
